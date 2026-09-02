dd247d577d5af96332ffee25d6bb6fcf17f6f9ff
Mohamed Gaber <jabrm6182@gmail.com>
2026-09-02 11:08:30 +0300

    creat FeaturedBooks Cubit state

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -28,7 +28,19 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<Item>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<Item>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoints: 'volumes?Filtering=free-ebooks&q=programming',
+      );
+      final bookModel = BookModel.fromJson(data);
+
+      return Right(bookModel.items ?? []);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.DioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
